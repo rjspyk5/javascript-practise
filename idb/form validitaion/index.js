8ae85502c7b1157a2d93bd3b9cf4d693df88432b
rjspyk5 <rjspyk5@gmail.com>
2024-02-06 17:23:01 +0600

    form validation file create and structure ready

---
+++ idb/form validitaion/index.js
@@ -0,0 +1,5 @@
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+const reset = document.getElementById("reset");
