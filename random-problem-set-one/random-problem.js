f084faebbc79b9777e787d71a832b23d093a2578
rjspyk5 <rjspyk5@gmail.com>
2024-02-07 03:11:57 +0600

    solved some problems

--- random-problem-set-one/random-problem.js
+++ random-problem-set-one/random-problem.js
@@ -107,145 +107,140 @@ b=temp;
 // // using destructing
 let c=50,d=600;
 [d,c]=[c,d]
+
 // // Fibonacchi
+const fibonacchi=(num)=>{
+  let first=0;
+  let second=1;
+  let sum=0;
+  for (let index = 0; index < num; index++) {
+    console.log(first);
+    sum=first+second;
+    
+    first=second;
+    second=sum;
+ 
+    
+  }
+}
+
+
 // // Shopping cart products
-// const products=[
-//   {name:"Chiruni",price:300,quantity:1},
-//   {name:"Chirun",price:5600,quantity:2}
-// ]
-// const total=products.reduce((acc,el)=>{
-//  return acc+(el.price*el.quantity);
-//   },0)
+
+const products=[
+  {name:"Chiruni",price:300,quantity:1},
+  {name:"Chirun",price:5600,quantity:2}
+]
+const total=products.reduce((acc,el)=>{
+ return acc+(el.price*el.quantity);
+  },0)
 
 //   // Discount Calculator
 //   // first 100=100;10-20=5;
+const discount=(quantity)=>{
+  if (quantity<=100) {
+    return quantity*100
+  } else if(quantity>100 && quantity<200){
+    let firstHundred=100*100;
+    let remaining=quantity-100;
+    return firstHundred+remaining*50
+    
+  }
+  else{
+    let remaining=quantity-200;
+    return remaining*quantity+firstHundred+(100*50)
+  }
+}
 
-//   let price;
-//   const discount=(quantity)=>{
-//     let remaining;
-//     let total;
-//     if (quantity>10) {
-//       remaining=quantity-10
-//       total=100;
-//       if (remaining>10) {
-//         remaining=remaining-10;
-//         total=total+50
-//        return total+(remaining*2)
-
-//       } else {
-//         return total+(remaining*5);
-
-//       }
-
-//     } else {
-//       return quantity*10
-
-//     }
-//   }
 
 //   array-looping-tasks
 //   Task 1
 //   Write a JavaScript code to reverse the array colors without using the reverse method.
+const arrayReverse=(arr)=>{
+  let newArray=[]
+  for (const el of arr) {
+    newArray.unshift(el)
 
-//   Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+    
+  }
+  return newArray;
+}
 
-//   Output:
 
-//   ['orange', 'yellow', 'green', 'blue', 'red']
 
 //   Task 2
 //   Write a JavaScript code to get the even numbers from an array using any looping technique.
+const getEvenNumber=(numbers)=>{
+  let evenArray=[]
+  for (const num of numbers) {
+    if (num%2===0) {
+      evenArray.push(num)
+      
+    }
 
-//   Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
-
-//   Output:
-
-//   [12, 98, 76, 46]
+    
+  }
+  return evenArray
+}
 
 //   Task 3
 //   Use a for...of loop to concatenate all the elements of an array into a single string.
-
-//   Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
-
-//   Output:
-
-//   'TomTimTinTik'
+const stringConcatation=(arr)=>{
+  let concatedArray="";
+  for (const el of arr) {
+    concatedArray+=el
+    
+  }
+  return concatedArray
+}
 
 //   Task 4 (Hard)
 //   Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+const reversedSentence=(str)=>{
 
-//   Input: const statement = 'I am a hard working person'
+  const splitedString=str.split(" ")
+ const reversed=[]
+for (const word of splitedString) {
+  reversed.unshift(word)
+  
+}
+return reversed.join(" ")
+}
 
-//   Output:
-
-//   'person working hard a am I'
-
-//   Simple function Related Practice Tasks
-// Task-1
-// Take four parameters. Multiply the four numbers and then return the result
-
-// Task-2
-// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
-
-// Task-3
-// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
-
-// Task-4
-// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
-
-// Task-5
-// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
-
-// js-problems-part1-practice-tasks
-// Task-1:
-// Write a function to convert temperature from Celsius to Fahrenheit.
-
-// Task-2:
-// You are given an array of numbers. Count how many times the a number is repeated in the array.
-
-// sample-input: numbers = [5,6,11,12,98, 5]
-
-// find: 5
-
-// output: 2
-
-// sample-input:
-
-// numbers = [5,6,11,12,98, 5]
-
-// find: 25
-
-// output: 0
-
-// Task-3:
-// Write a function to count the number of vowels in a string.
-
-// Task-4:
-// Write a function to find the longest word in a given string.
-
-// sample-input: I am learning Programming to become a programmer
-
-// sample-output: Programming
-
-// Task-5:
-// Generate a random number between 10 to 20.
-
-// js-problems-part2-practice-tasks
-// Task -1:
 // Find the lowest number in the array below.
-// const heights2 = [167, 190, 120, 165, 137];
+
+const lowestFinder=(arr)=>{
+  let low=ar[0]
+  for (const el of arr) {
+    if (el<low) {
+      low=el
+      
+    }
+    
+  }
+  return low
+}
 
 // Task -2:
 // Find the friend with the smallest name.
 // const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+const smallestStringFinder=(arr)=>{
+  let smallest=arr[0]
+  for (const el of arr) {
+    if (el.length<smallest.length) {
+      smallest=el
+      
+    }
+    
+  }
+  return smallest
+}
 
-// Task-3:
-// Your task is to calculate the total budget required to buy electronics:
 
-//     laptop = 35000 tk
-//     tablet = 15000 tk
-//     mobile = 20000 tk
 // Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
+const totalBudget=(laptopQuantity,tabletQuantity,mobileQuantity)=>{
+  return (laptopQuantity*35000)+(tabletQuantity*15000)+(mobileQuantity*20000)
+}
 
 // Task-4:
 // You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
@@ -269,3 +264,5 @@ let c=50,d=600;
 //             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
 //             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
 //         ];
+// Task-4
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
