e3f8c81162f5bc01da3224c8d834b338e22a5216
rjspyk5 <rjspyk5@gmail.com>
2024-02-06 19:02:55 +0600

    update user registration

--- idb/form validitaion/index.js
+++ idb/form validitaion/index.js
@@ -3,3 +3,9 @@ const lastName = document.getElementById("lastname");
 const password = document.getElementById("password");
 const submit = document.getElementById("submit");
 const reset = document.getElementById("reset");
+// Submit Handle
+const handleSubmit = () => {
+  console.log("hello world");
+};
+
+submit.addEventListener("click", () => handleSubmit());
